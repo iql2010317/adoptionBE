a881d7a63b793ce1a0503af692d94c08c4526e65
iql2010317 <iql2010317@gmail.com>
2023-12-14 16:24:24 +0800

    12.14 權限+後台管理

--- src/test/java/com/example/adoption/AdoptionApplicationTests.java
+++ src/test/java/com/example/adoption/AdoptionApplicationTests.java
@@ -33,10 +33,10 @@ class AdoptionApplicationTests {
 //		userInfo.setPassword("test");
 //		userInfo.setPhone("0977456874");
 //		userInfo.setEmail("test@gamil.com");
-		LocalDate birth = LocalDate.parse("2001-05-05");
+		LocalDate birth = LocalDate.parse("1995-03-13");
 //		userInfo.setAccount("haha");
-		userInfo.setPassword("test01");
-		userInfo.setEmail("test01@mail.com");
+		userInfo.setPassword("123");
+		userInfo.setEmail("test99");
 		userInfo.setBirthday(birth);
 
 		UserInfoRequest req = new UserInfoRequest(userInfo);
@@ -46,14 +46,14 @@ class AdoptionApplicationTests {
 	@Test
 	void userInfoUpdateTest() {
 		// 模擬已存在於資料庫中的 UserInfo
-		int userIdToUpdate = 4; // 假設需要更新的用戶ID為1
+		int userIdToUpdate = 13; // 假設需要更新的用戶ID為1
 		UserInfo existingUserInfo = userInfoDao.findById(userIdToUpdate).orElse(null);
 
 		// 假設在資料庫中找到了該用戶信息
 		assertNotNull(existingUserInfo);
 
 		// 更新該使用者的資料
-		existingUserInfo.setUserName("Updated Name3"); // 更新用戶名稱
+		existingUserInfo.setUserName("翔"); // 更新用戶名稱
 
 		// 創建一個 UserInfoRequest 物件
 		UserInfoRequest req = new UserInfoRequest(existingUserInfo);
